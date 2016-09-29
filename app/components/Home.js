438d7c5c9d193a2e92576193cf6712d68a12d380
Paulo Cesar Junior <paulocesarpcfj@gmail.com>
2016-09-29 14:46:41 -0300

    Init Commit

---
+++ app/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => (
+	<div>
+		<h1>teste</h1>
+		<h2>teste2</h2>
+	</div>
+)
+
+
+export default Home;
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    entry: './app/app.js',
+    output: {
+        path: './public',
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+	    inline: true,
+	    contentBase: './public',
+	    port: 3000
+	},
+
+	module: {
+		loaders: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			loader: 'babel',
+			query: {
+				presets: ['es2015', 'react']
+			}
+		}]
+	}
+}
